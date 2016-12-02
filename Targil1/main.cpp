a8f8d7201df395a73d1bec7d158c59fe650514d7
Dor Mitzafon <dor.m@dor-m-mbp.local>
2016-12-02 11:59:24 +0200

    Initial Commit

---
+++ Targil1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Targil1
+//
+//  Created by Dor Mitzafon on 02/12/2016.
+//  Copyright © 2016 Dor Mitzafon. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
